c605ae145d614149d3740ab2598977ac3e620efc
CRASHFIRE666 <54841369+CRASHFIRE-666@users.noreply.github.com>
2019-12-24 13:30:10 +0300

    Update 11.6.cpp

--- 11.6.cpp
+++ 11.6.cpp
@@ -1,21 +1,48 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+int findIndex(int n){
+    int a = 1, b = 1, c = 2;
+    int res = 2;
+    
+    while (c < n){
+        c = a + b;
+        res++;
+        a = b;
+        b = c;
+    }
+    if (c > n){
+    return 0;
+    }
+    else{
+        return res;
+    }
+}
+
+
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    int n,k;
-    int i=2;
-    cout<<"Введите число"<<endl;
-    cin>>n;
-    int a=0;
-    int b=1;
-    while (b!=n)
-    {
-        k=b;
-        b=a+b;
-        a=k;
-        i++;
+    int n;
+    cin >> n;
+    if (n > 1){
+        if (n == 2){
+            cout << 3 << endl;
+        }
+        else{
+            int result = findIndex(n);
+            if(result != 0){
+                cout << result << endl;
+            }
+            else{
+                cout << "НЕПРАВИЛЬНОЕ ЗНАЧЕНИЕ" << endl;
+            }
+        }
     }
-    cout<<"К="<<i-1<<endl;
-
+    else{
+        cout << "НЕПРАВИЛЬНОЕ ЗНАЧЕНИЕ" << endl;
+    }
+    return 0;
 }
